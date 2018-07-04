0481b5a5803f47dbc043ea8705a640112c9c405b
Haiyong Xu <haiyeong@gmail.com>
2018-07-04 00:40:37 -0400

    change audio-alignment to be a library, integret into parent Makefile

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <libgen.h>
 #include "FFmpegDemuxer.h"
 #include "FFmpegMuxer.h"
+#include "framework/mlt.h"
+#include "audio-alignment/audioCorrelation.h"
 
 using namespace std;
 using namespace Swaper;
@@ -18,7 +20,52 @@ int main(int argc, char* argv[])
   string filename2 = argv[2];
   string filename3 = argv[3];
 
-  FFmpegMuxer::mux(filename1, filename2, filename3);
+
+    // Initialise the factory
+    if ( mlt_factory_init( NULL ) != 0 )
+    {
+        cout << "failed to init mlt factory." << endl;
+        return EXIT_FAILURE;
+    }
+
+    // Create the default consumer
+    //mlt_consumer hello = mlt_factory_consumer( NULL, NULL );
+
+    // Create via the default producer
+    mlt_producer in_1 = mlt_factory_producer( NULL, nullptr, argv[ 1 ] );
+    mlt_producer in_2 = mlt_factory_producer( NULL, nullptr, argv[ 2 ] );
+
+    Mlt::Producer prod_1(in_1);
+    Mlt::Producer prod_2(in_2);
+    /*
+    // Connect the producer to the consumer
+    mlt_consumer_connect( hello, mlt_producer_service( world ) );
+
+    // Start the consumer
+    mlt_consumer_start( hello );
+
+    // Wait for the consumer to terminate
+    while( !mlt_consumer_is_stopped( hello ) )
+        sleep( 1 );
+
+    // Close the consumer
+    mlt_consumer_close( hello );
+
+    // Close the producer
+    mlt_producer_close( world );
+
+    // Close the factory
+    mlt_factory_close( );
+    */
+
+
+  AudioEnvelope audioEnv_1(QString(filename1.c_str()), &prod_1);
+  AudioEnvelope audioEnv_2(QString(filename2.c_str()), &prod_2);
+
+  AudioCorrelation audioCorr(&audioEnv_1);
+  audioCorr.addChild(&audioEnv_2);
+
+  //FFmpegMuxer::mux(filename1, filename2, filename3);
 
   return 0;
 }
